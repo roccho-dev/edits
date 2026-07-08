413e6f5087afa8cc969a0e57e471e1c6b66ec8b4
roccho <40359643+roccho-dev@users.noreply.github.com>
2026-07-08 10:45:57 +0900

    windows: keep pty proof buildable on non-unix

--- packages/hq-pty-vim-rsc/internal/pty/openpty_stub.go
+++ packages/hq-pty-vim-rsc/internal/pty/openpty_stub.go
@@ -2,13 +2,21 @@
 
 package pty
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
-type Session struct{}
+type Session struct{ Master *os.File }
 
 func Start(argv []string, env []string) (*Session, error) {
 	return nil, fmt.Errorf("pty proof requires unix+cgo")
 }
-func (s *Session) PID() int     { return 0 }
-func (s *Session) Wait() error  { return nil }
-func (s *Session) Close() error { return nil }
+func (s *Session) PID() int    { return 0 }
+func (s *Session) Wait() error { return nil }
+func (s *Session) Close() error {
+	if s != nil && s.Master != nil {
+		_ = s.Master.Close()
+	}
+	return nil
+}
